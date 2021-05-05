98db821c579c95c70bf1d330e8b98597990517c5
susangeomtech <83689602+susangeomtech@users.noreply.github.com>
2021-05-05 11:22:07 +0530

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,24 @@
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+print(counter)
+print(miles)
+print(name)
+print("Aum")
+
+mystring="Don't worry about spostrophes"
+print(mystring)
+
+hello="hello"
+world="world"
+helloworld=hello + " " + world
+print(helloworld)
+
+a,b=3, 4
+print(a,b)
+print("a is :", a)
+
+squared = 7**2
+cubed=2**3
+print(squared)
+print(cubed)
