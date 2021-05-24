5a71206f047819afb87b20b97bb244c876087c06
susangeomtech <geomtech2020@gmail.com>
2021-05-24 21:25:03 +0000

    From GCP

--- set_data_to_db.py
+++ set_data_to_db.py
@@ -8,13 +8,20 @@ OPEN_WEATHER_MAP_API_KEY = "31ddfd85c327437703aecdba29571796"
 api_url = "http://api.openweathermap.org/data/2.5/weather?q=Bengaluru&appid=" + OPEN_WEATHER_MAP_API_KEY
 
 app = Flask(__name__)
+@app.route(/)
+def welcome():
+  return "Welcome to Amrita Intelligent Infrastructure Data Management and Control Panel App. \n Use one of the options below."
+
+@app.route('/hello')
+def  hello():
+  return "Hello"
+
 @app.route('/getWeatherData')
 def get_open_weather_map_data():
   response = requests.get(api_url).json()
   print(response)
   return response
 
-app = Flask(__name__)
 @app.route('/setWeatherDataToDb')
 def set_db_data():
   response = get_open_weather_map_data()
